ac9083798bfafa56cfa9292e3c2ff614639d8170
Fonseka M.M.N.H <it21156410@my.sliit.lk>
2023-10-28 02:27:30 +0530

    splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../utils/auth_checker.dart';
-
+// SplashScreen
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
